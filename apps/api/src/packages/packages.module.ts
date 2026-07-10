cba88cc2eacd5ba4718a63844652d604d9bdb087
Elyas Karasik <karasikelyasik@gmail.com>
2026-07-10 17:32:17 +0200

    Fix Prisma dependency in packages module

--- apps/api/src/packages/packages.module.ts
+++ apps/api/src/packages/packages.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PackagesController } from './packages.controller';
 import { PackagesService } from './packages.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [PackagesController],
-  providers: [PackagesService],
+  providers: [
+    PackagesService,
+    PrismaService,
+  ],
   exports: [PackagesService],
 })
 export class PackagesModule {}

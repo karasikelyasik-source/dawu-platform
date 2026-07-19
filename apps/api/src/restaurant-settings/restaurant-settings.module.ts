be600c12a2edcf40e004217ee5329425ec0d5c82
Elyas Karasik <karasikelyasik@gmail.com>
2026-07-19 15:45:20 +0200

    Add restaurant settings API

--- apps/api/src/restaurant-settings/restaurant-settings.module.ts
+++ apps/api/src/restaurant-settings/restaurant-settings.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaService } from '../prisma/prisma.service';
 import { RestaurantSettingsController } from './restaurant-settings.controller';
 import { RestaurantSettingsService } from './restaurant-settings.service';
 
 @Module({
-  imports: [
-    PrismaModule,
-  ],
   controllers: [
     RestaurantSettingsController,
   ],
   providers: [
+    PrismaService,
     RestaurantSettingsService,
   ],
   exports: [
